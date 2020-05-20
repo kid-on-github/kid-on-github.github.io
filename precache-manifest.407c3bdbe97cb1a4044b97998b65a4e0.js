978432e5b6e9ed22e11ade278fe42a11d595dc31
Mike <mike@lemonshell.com>
2020-05-20 13:50:13 -0700

    updated with ReactJS code

---
+++ precache-manifest.407c3bdbe97cb1a4044b97998b65a4e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d859d74ee5660647d083ae4563ed017b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb3e16c78b8a3c8dfdff",
+    "url": "/static/css/main.b09c1d76.chunk.css"
+  },
+  {
+    "revision": "0e642afae6fd7e495b9f",
+    "url": "/static/js/2.27ae849e.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/static/js/2.27ae849e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3e16c78b8a3c8dfdff",
+    "url": "/static/js/main.02163e50.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
